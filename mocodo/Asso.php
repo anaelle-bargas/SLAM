a00ec7d744adfa92fe76c3a4c139405e807bc862
unknown <anaelle.bargas@sts-sio-caen.info>
2024-05-14 18:23:09 +0200

    continuing

--- mocodo/Asso.php
+++ mocodo/Asso.php
@@ -7,20 +7,33 @@ class Asso{
     public $typeAsso;
     public $champs;
 
+    
     public function __construct($ligne)
     {
         echo "Asso";
-        $elems = explode(", ", $ligne);
-        $this->nom = $elems[0];
-        
-        $this->cardTables = $this->cardTables($elems);
-        $this->typeAsso = $this->typeAsso($this->cardTables);
+        $explode = explode(", ", $ligne);
+        $this->nom = $explode[0];
+        $this->cardTables = $this->cardTables(array_slice($explode, 1));
+        $this->typeAsso = $this->typeAsso();
     }
     public function cardTables($elems){
-        echo print_r($elems);
+        echo "<br> cardinalités <br>";
+        foreach($elems as $l){
+
+            $liens = explode(" ", $l);
+            $cards = array();
+            $origins = array();
+            $lenChamps = count($liens);
+            for($i=0;$i<$lenChamps;$i++){
+                if(ctype_digit($liens[$i][0])){
+                    echo "nombre".$liens[$i][0];
+                }
+            }
+            echo print_r($elems);
+        }
     }
-    public function typeAsso($cardTables){
-        echo "typeAsso cardTables".$cardTables;
+    public function typeAsso(){
+        echo "typeAsso cardTables";
 
     }
 }
--- mocodo/Table.php
+++ mocodo/Table.php
@@ -4,22 +4,44 @@ namespace SLAM\mocodo;
 class Table {
     public $nom;
     public $champs;
+    public $sql;
 
     public function __construct($ligne)
     {
         echo "Table";
-        $this->nom ="";
-        $this->champs = array();
+        $this->nom =$this->setNom($ligne);
+        $this->champs = $this->addChamps($ligne);
+        echo "<br> ligne ".$ligne;
+        echo "<br> nomTable ".$this->nom;
+        echo "<br> champsTable <br>";
+        print_r($this->champs);
+        $this->sql=$this->setCode();
+        echo "<br> sql code ".$this->sql;
+        echo "<br> FinTable <br>";
 
+    }
+
+    public function setCode(){
+        $code= "CREATE TABLE ".$this->nom."(<br>";
+        $cs = $this->champs;
+        $lenChamps = count($cs);
+        for($i=0;$i<$lenChamps;$i++){
+            $code.=$cs[$i].(($i==0)?" PRIMARY KEY, <br>":(($i==$lenChamps-1)?"<br>":",<br>"));
+        }
+        foreach($this->champs as $c){
+        }
+        return $code.");";
     }
     
 
     public function setNom($ligne){
-        $nom=explode(":", $ligne);
-        $this->nom =$nom[0];
+        return explode(":", $ligne)[0];
     }
 
-    public function addChamps($champ){
-        array_push($this->champs, $champ);
+    public function addChamps($ligne){
+        $champs = explode(": ", $ligne)[1];
+        return explode(", ", $champs);
     }
+
+
 }
